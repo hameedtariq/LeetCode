14197a076d88dd3fcd386ad324adf60ee97a5553
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2023-12-01 17:17:14 +0500

    Time: 20 ms (97.87%) | Memory: 12.9 MB (56.44%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,29 +1,40 @@
 class Solution {
+private:
+    int m;
+    int n;
+    const vector<vector<int>> directions = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+    
 public:
-    const vector<pair<int,int>> dv = {make_pair(0,1),make_pair(1,0),make_pair(-1,0), make_pair(0,-1)};
-    int numIslands(vector<vector<char>>& map) {
+    int numIslands(vector<vector<char>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
         int count = 0;
-        vector<vector<bool>> visited(map.size(), vector<bool>(map[0].size(), false));
-        for(int i =0; i< map.size(); i++){
-            for(int j =0; j< map[0].size(); j++){ // atleast n^2 to visit all nodes.
-                if(visited[i][j] || map[i][j] == '0') continue;
-                dfs(map,visited,i,j); // in worst case all are ones so, it will n^2 but if this is n^2 then it wont be called again.
-                count++;
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '1') {
+                    ++count;
+                    dfs(grid, i, j);
+                }
             }
         }
+        
         return count;
     }
-    void dfs(vector<vector<char>>& map, vector<vector<bool>>& visited, int i, int j){
-        if(map[i][j] == '0') return;
-        visited[i][j] = true;
-        for(auto& v: dv) {
-            // bound checking
-            if(i+v.first < 0 || i+v.first >= map.size()) continue;
-            if(j+v.second < 0 || j+v.second >= map[0].size()) continue;
-
-            if(!visited[i+v.first][j+v.second]){
-                dfs(map,visited,i+v.first,j+v.second);
-            }
+    
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+        grid[row][col] = '0';
+        
+        for (const auto& direction : directions) {
+            int nextRow = row + direction[0];
+            int nextCol = col + direction[1];
+            
+            if (isValid(grid, nextRow, nextCol) && grid[nextRow][nextCol] == '1')
+                dfs(grid, nextRow, nextCol);
         }
+    }
+    
+    bool isValid(vector<vector<char>>& grid, int row, int col) {
+        return row >= 0 && col >= 0 && row < m && col < n;
     }
 };
