fdd5d623b8b34fe2ec4c88b650c5ed89e9f5fa62
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2023-12-01 17:16:38 +0500

    Time: 38 ms (21.83%) | Memory: 13 MB (56.44%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,25 +3,26 @@ public:
     const vector<pair<int,int>> dv = {make_pair(0,1),make_pair(1,0),make_pair(-1,0), make_pair(0,-1)};
     int numIslands(vector<vector<char>>& map) {
         int count = 0;
+        vector<vector<bool>> visited(map.size(), vector<bool>(map[0].size(), false));
         for(int i =0; i< map.size(); i++){
             for(int j =0; j< map[0].size(); j++){ // atleast n^2 to visit all nodes.
-                if(map[i][j] == '0') continue;
-                dfs(map,i,j); // in worst case all are ones so, it will n^2 but if this is n^2 then it wont be called again.
+                if(visited[i][j] || map[i][j] == '0') continue;
+                dfs(map,visited,i,j); // in worst case all are ones so, it will n^2 but if this is n^2 then it wont be called again.
                 count++;
             }
         }
         return count;
     }
-    void dfs(vector<vector<char>>& map, int i, int j){
+    void dfs(vector<vector<char>>& map, vector<vector<bool>>& visited, int i, int j){
         if(map[i][j] == '0') return;
-        map[i][j] = '0';
+        visited[i][j] = true;
         for(auto& v: dv) {
             // bound checking
             if(i+v.first < 0 || i+v.first >= map.size()) continue;
             if(j+v.second < 0 || j+v.second >= map[0].size()) continue;
 
-            if(map[i+v.first][j+v.second] != '0'){
-                dfs(map,i+v.first,j+v.second);
+            if(!visited[i+v.first][j+v.second]){
+                dfs(map,visited,i+v.first,j+v.second);
             }
         }
     }
