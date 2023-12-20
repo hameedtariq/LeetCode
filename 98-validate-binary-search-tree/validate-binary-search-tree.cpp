00caa2197218131b165192ae1ee8132159971e5d
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2023-12-20 17:56:44 +0500

    Time: 9 ms (49.16%) | Memory: 12.5 MB (99.88%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -9,12 +9,15 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ bool _ = []{ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);return false;}();
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
         int prev = INT_MAX;
         bool hit = false;
-        return inorder(root,prev,hit);
+        hit = inorder(root,prev,hit);
+        root->left=root->right=nullptr;
+        return hit;
     }
     bool inorder(TreeNode* root, int& prev, bool& hit){
         if(!root) return true;
