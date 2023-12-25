47d6b5ab2a044fd9ab36be3f664f27d2adf934c4
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2023-12-25 23:51:19 +0500

    Time: 42 ms (19.26%) | Memory: 34.3 MB (5.64%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,19 +1,39 @@
+#include <vector>
+#include <climits>
+
 class Solution {
-    bool _ = [](){ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr); return false;}();
 public:
-    int change(int amount, vector<int>& coins) {
-        // unordered_set<int> du;
-        vector<vector<int>> memo(amount+1, vector<int>(coins.size(),-1));
-        return rec(amount,coins,0,memo);
-    }
-    int rec(int amount, vector<int>& coins,int start,vector<vector<int>>& memo){
-        if(amount == 0) return 1;
-        if(amount < 0) return 0;   
-        if(memo[amount][start] != -1) return memo[amount][start];     
-        memo[amount][start] = 0;
-        for(int i = start; i < coins.size(); i++){
-            memo[amount][start] += rec(amount-coins[i],coins,i,memo);
+    int64_t solve(vector<int>& coins, int target, int index, vector<vector<int64_t>>& dp) {
+        if (target == 0) {
+            return 1;
         }
-        return memo[amount][start];
+        if (index == coins.size()) {
+            return 0;
+        }
+        if (target < 0) {
+            return 0;
+        }
+
+        if (dp[target][index] != -1) {
+            return dp[target][index];
+        }
+
+        int64_t include = solve(coins, target - coins[index], index, dp);
+        int64_t exclude = solve(coins, target, index + 1, dp);
+
+        dp[target][index] = include + exclude;
+        return dp[target][index];
+    }
+
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int64_t>> dp(amount + 1, vector<int64_t>(coins.size() + 1, -1));
+        int64_t ways = solve(coins, amount, 0, dp);
+
+        // Check if the result fits within the range of a 32-bit signed integer
+        if (ways > INT_MAX) {
+            return INT_MAX;
+        }
+
+        return static_cast<int>(ways);
     }
 };
