712f9dd3a802b7d4f441d21ba0083271c533ac80
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2023-12-25 23:44:24 +0500

    Time: 628 ms (5.03%) | Memory: 22.2 MB (10.89%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        // unordered_set<int> du;
+        vector<vector<int>> memo(amount+1, vector<int>(coins.size(),-1));
+        return rec(amount,coins,0,memo);
+    }
+    int rec(int amount, vector<int>& coins,int start,vector<vector<int>>& memo){
+        if(amount == 0) return 1;
+        if(amount < 0) return 0;   
+        if(memo[amount][start] != -1) return memo[amount][start];     
+        memo[amount][start] = 0;
+        for(int i = start; i < coins.size(); i++){
+            memo[amount][start] += rec(amount-coins[i],coins,i,memo);
+        }
+        return memo[amount][start];
+    }
+};
