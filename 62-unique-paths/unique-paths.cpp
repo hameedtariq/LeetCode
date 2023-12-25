5eb9e1cf4c90ee15ebcc643abe6781a3db96445d
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2023-12-25 14:19:26 +0500

    Time: 0 ms (100.00%) | Memory: 7.1 MB (9.50%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,59 +1,19 @@
+bool _ = []{ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);return false;}();
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        //return uniquePathsTopDown(m, n);
-        return uniquePathsBottomUp(m, n);
-    }
-    
-    int uniquePathsTopDown(int m, int n) {
-        this->m = m;
-        this->n = n;
-        this->cache = vector(m, vector<int>(n, 0));
-        return dp(m-1, n-1);
-    }
-    
-    int m;
-    int n;
-    vector<vector<int>> cache;
-    int dp(int row, int col) {
-        if (row == 0 && col == 0) {
-            return 1;
+        vector<vector<int>> memo(m,vector<int>(n,0));
+        for(int i =1; i<=m; i++){
+            memo[m-i][n-1] = 1;
         }
-        
-        if (cache[row][col] == 0) {
-            int nbPaths = 0;
-            if (row > 0) {
-                nbPaths += dp(row-1, col);
-            }
-            if (col > 0) {
-                nbPaths += dp(row, col-1);
-            } 
-            cache[row][col] = nbPaths;
+        for(int i =1; i<=n; i++){
+            memo[m-1][n-i] = 1;
         }
-        
-        return cache[row][col];    
-    }
-    
-    int uniquePathsBottomUp(int m, int n) {
-        vector<vector<int>> cache(m, vector<int>(n, 0));
-        cache[0][0] = 1;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    continue;
-                }
-                int nbPaths = 0;
-                if (i > 0) {
-                    nbPaths += cache[i-1][j];
-                }
-                if (j > 0) {
-                    nbPaths += cache[i][j-1];
-                }
-                cache[i][j] = nbPaths;
+        for(int i = m-2; i>=0; i--){
+            for(int j = n-2; j>=0; j--){
+                memo[i][j] = memo[i+1][j] + memo[i][j+1]; 
             }
         }
-
-        return cache[m-1][n-1];
+        return memo[0][0];
     }
 };
