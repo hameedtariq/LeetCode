9ed3ced62fa361ceb65476ee397465dd44e33af1
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2023-12-25 14:29:37 +0500

    Time: 2 ms (43.39%) | Memory: 6.5 MB (78.31%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -2,16 +2,18 @@ bool _ = []{ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);return false;}();
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> memo(m,vector<int>(n,0));
-        for(int i =1; i<=m; i++){
-            memo[m-i][n-1] = 1;
-        }
+        if(m==1) return 1;
+        vector<vector<int>> memo(2,vector<int>(n,0));
         for(int i =1; i<=n; i++){
-            memo[m-1][n-i] = 1;
+            memo[1][n-i] = 1;
         }
+        memo[0][n-1] = 1;
         for(int i = m-2; i>=0; i--){
             for(int j = n-2; j>=0; j--){
-                memo[i][j] = memo[i+1][j] + memo[i][j+1]; 
+                memo[0][j] = memo[1][j] + memo[0][j+1]; 
+            }
+            for(int i =1; i<=n; i++){
+                memo[1][n-i] = memo[0][n-i];
             }
         }
         return memo[0][0];
