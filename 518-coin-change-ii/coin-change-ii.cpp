bba0010f82a4f25352f001868705b72f790ccadb
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2023-12-26 22:56:57 +0500

    Time: 44 ms (16.55%) | Memory: 22.5 MB (8.96%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,39 +1,20 @@
-#include <vector>
-#include <climits>
-
 class Solution {
+    bool _ = [](){ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr); return false;}();
 public:
-    int64_t solve(vector<int>& coins, int target, int index, vector<vector<int64_t>>& dp) {
-        if (target == 0) {
-            return 1;
-        }
-        if (index == coins.size()) {
-            return 0;
-        }
-        if (target < 0) {
-            return 0;
-        }
-
-        if (dp[target][index] != -1) {
-            return dp[target][index];
-        }
-
-        int64_t include = solve(coins, target - coins[index], index, dp);
-        int64_t exclude = solve(coins, target, index + 1, dp);
-
-        dp[target][index] = include + exclude;
-        return dp[target][index];
-    }
-
     int change(int amount, vector<int>& coins) {
-        vector<vector<int64_t>> dp(amount + 1, vector<int64_t>(coins.size() + 1, -1));
-        int64_t ways = solve(coins, amount, 0, dp);
+        // unordered_set<int> du;
+        vector<vector<int>> memo(amount+1, vector<int>(coins.size(),-1));
+        return rec(amount,coins,0,memo);
+    }
+    int rec(int amount, vector<int>& coins,int start,vector<vector<int>>& memo){
+        if(amount == 0) return 1;
+        if(amount < 0) return 0;   
+        if(start >= coins.size()) return 0;
+        if(memo[amount][start] != -1) return memo[amount][start];     
 
-        // Check if the result fits within the range of a 32-bit signed integer
-        if (ways > INT_MAX) {
-            return INT_MAX;
-        }
-
-        return static_cast<int>(ways);
+        int include = rec(amount-coins[start],coins,start,memo);
+        int exclude = rec(amount,coins,start+1,memo);
+        memo[amount][start] = include + exclude;
+        return memo[amount][start];
     }
 };
