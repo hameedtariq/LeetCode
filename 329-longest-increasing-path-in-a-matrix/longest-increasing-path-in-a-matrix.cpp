d1d992437987dba1618cf9ef0f2f69a306174ba9
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2023-12-29 19:56:04 +0500

    Time: 52 ms (32.41%) | Memory: 16.8 MB (39.63%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    vector<vector<int>> dv = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int path = -1;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        vector<vector<int>> memo(m,vector<int>(n,-1));
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                path = max(path, dfs(matrix,visited,i,j,-1, memo));
+            }
+        }
+        return path;
+    }
+    int dfs(vector<vector<int>>& m, vector<vector<bool>>& v, int i,int j, int prev, vector<vector<int>> &memo) {
+        if(i >= m.size() || i < 0 || j >= m[0].size() || j < 0 || v[i][j]) {
+            return 0;
+        }
+        if(m[i][j] <= prev) return 0;
+        if(memo[i][j] != -1) return memo[i][j];
+        v[i][j] = true;
+        int path = -1;
+        for(auto& x: dv){
+            path = max(path,dfs(m,v,i+x[0],j+x[1], m[i][j],memo) + 1);
+        }
+        v[i][j] = false;
+        return memo[i][j] = path;
+    }
+};
