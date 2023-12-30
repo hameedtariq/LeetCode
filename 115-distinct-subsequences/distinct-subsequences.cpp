54822ff3afebe7c3428d76e62aaacfbbec0d170d
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2023-12-30 22:17:21 +0500

    Time: 34 ms (57.00%) | Memory: 18.8 MB (58.66%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
-    int numDistinct(string s, string t) {
-        vector<vector<int>> memo(s.size(),vector<int>(t.size(), -1));
-        return rec(s,t,0,0,memo);
+    int solve(string &s,int i,int j,string &t,vector<vector<int>>&dp)
+{
+    if(j==t.size()-1)return 1;
+    if(i>=s.size())return 0;
+
+    if(dp[i][j]!=-1)return dp[i][j];
+    
+    int ans=0;
+    if(s[i]==t[j])
+    {
+        for(int z=i+1;z<s.size();z++)
+        {
+            if(t[j+1]==s[z]&&s.size()-z>=t.size()-j-1)
+            {
+                ans+=solve(s,z,j+1,t,dp);
+            }
+            
+        }
     }
-    int rec(string& s, string& t, int i, int j,vector<vector<int>> &memo){
-        if(j == t.size()){
-            return 1;
-        }
-        if(i == s.size()){
-            return 0;
-        }
-        if(memo[i][j] != -1) return memo[i][j];
-        int ans = 0;
-        if(s[i] == t[j]) {
-            ans += rec(s,t, i+1,j+1,memo);
-        }
-        ans += rec(s,t,i+1,j,memo);
-        return memo[i][j] = ans;
+    return dp[i][j]=ans;
+}
+
+int numDistinct(string s, string t) {
+    
+    int ans=0;
+    vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
+
+    for(int z=0;z<s.size();z++)
+    {
+        if(s[z]==t[0]&&s.size()-z>=t.size())
+            ans+=solve(s,z,0,t,dp);
     }
-};
+    return ans;
+}
+};  
