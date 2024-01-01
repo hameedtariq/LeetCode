05ec450c784f9b6b86f79d1d9c4ce72029942518
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-01-01 22:10:59 +0500

    Time: 28 ms (5.92%) | Memory: 13.7 MB (99.24%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,21 +1,24 @@
 bool _ = []{ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);return false;}();
 class Solution {
 public:
+    struct myComp {
+        constexpr bool operator()(pair<int, int> const& a,pair<int, int> const& b) const noexcept {
+            return a.second < b.second;
+        }
+    };
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> count;
-        for(auto&a: nums) {
-            count[a]++;
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<int> ans(k,-1);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, myComp> pq;
+        for(int i =0; i< n;) {
+            int j =i+1;
+            for(; j<n && nums[j] == nums[j-1]; j++){};
+            pq.push({nums[i], j-i});
+            i=j;
         }
-        vector<pair<int,int>> ss;
-        for(auto& x: count){
-            ss.push_back(x);
-        }
-        sort(ss.begin(),ss.end(), [](pair<int,int>& a, pair<int,int>& b){
-            return a.second > b.second;
-        });
-        vector<int> ans(k);
-        for(int i =0; i<k; i++){
-            ans[i] = ss[i].first;
+        for(int i=0; i<k;i++){
+            ans[i] = pq.top().first;pq.pop();
         }
         return ans;
     }
