9a5fc2837a060ca3069c0e877f011e3f74fce3d4
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-01-07 02:02:03 +0500

    Time: 179 ms (19.22%) | Memory: 72.5 MB (10.57%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(n==trust.size()) return -1;
+        unordered_map<int,vector<int>> graph;
+        unordered_map<int,vector<int>> graph2;
+        
+        for(int i =0; i< trust.size(); i++){
+            graph[trust[i][0]].push_back(trust[i][1]);
+            graph2[trust[i][1]].push_back(trust[i][0]);
+        }
+        for(int i =1; i<= n; i++){
+            if(graph[i].size() == 0){
+                if(graph2[i].size() == n-1) return i;
+            }
+        }
+        
+        return -1;
+    }
+};
