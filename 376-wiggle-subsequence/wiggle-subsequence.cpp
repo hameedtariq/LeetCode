98e6787ab5fb77e8e45e259ba3acad4ebcc0773f
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-01-24 13:55:28 +0500

    Time: 0 ms (100.00%) | Memory: 8.9 MB (11.95%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -23,7 +23,6 @@ public:
         while(index < size && nums[index] == 0) index++; // skip zeros
         if(index == size) return count;
         bool pos = (nums[index] > 0); // false
-        cout << "Starting: " << pos << endl;
         for(;index < size; index++){
             if(nums[index] > 0 && !pos || nums[index] < 0 && pos) {
                 count++;
@@ -31,32 +30,5 @@ public:
             }
         }
         return count+1;
-
-
-
-
-
-
-
-
-
-
-        // int i = 1;
-        // // skip any duplicates in the start
-        // while(i < size && nums[i]-nums[i-1] ==0) i++;
-        // if(i == size) return 1;
-        // bool pos = (nums[i] - nums[i-1] > 0);
-        // int count = 1;
-        // int index = i - 1;
-        // for(; i< nums.size(); i++){
-        //     int diff = nums[i] - nums[index];
-        //     if(pos && diff > 0 || !pos && diff < 0){
-        //         cout << pos << " ";
-        //         count++;
-        //         pos = !pos;
-        //         index = i;
-        //     }
-        // }
-        // return count;
     }
 };
