bb2f24b7f296bef06476d3651f97b47d69bc0fda
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-01-25 12:57:35 +0500

    Time: 7 ms (100.00%) | Memory: 28.3 MB (6.59%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.cpp
+++ 495-teemo-attacking/teemo-attacking.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         int n = timeSeries.size();
         int total = duration;
         for(int i = 1; i<n; i++){
