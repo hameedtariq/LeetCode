5efc1e528978e7930d5393fd544c311bfe318467
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-02-03 17:31:48 +0500

    Time: 5 ms (33.91%) | Memory: 10.3 MB (10.59%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -1,55 +1,50 @@
 class Solution {
 public:
     int matrixScore(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        // do row flips
-        for(int i = 0;i <rows; i++) {
-            if(grid[i][0] == 0) {
-                cout << "Flipped Row: " << i << endl; 
-                flip(grid, i);
-            }
-        }
-        // do column flips
-        for(int i = 0;i <cols; i++) {
-            int count = 0; // tracks ones in ith column 
-            for(int j=0; j<rows; j++){
-                if(grid[j][i] == 1) count++;
-            }
-            if(count < ceil((float)rows/2)) {
-                cout << "Flipped Col: " << i << endl; 
-                flip(grid, i, false);
-            }
-        }
-        int ans = 0;
-        for(int i = 0;i <rows; i++) {
-            ans += binaryToDecimal(grid[i]);
-        }
-        return ans;
-    }
-    void flip(vector<vector<int>>& grid, int k, bool row = true) {
-        if(row) {
-            int rowSize = grid[0].size();
-            for(int i =0; i<rowSize; i++) {
-                grid[k][i] = !grid[k][i];
-            }
-        }else {
-            int colSize = grid.size();
-            for(int i =0; i<colSize; i++) {
-                grid[i][k] = !grid[i][k];
-            }
-        }
-    }
-    int binaryToDecimal(const vector<int>& binaryVector) {
-        int decimal = 0;
-        int power = 1;
+        int sum=0;
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        for(int i=0;i<m;i++){
+            if(grid[i][0]==0){
+                for(int j=0;j<n;j++){
+                    if(grid[i][j]==0)
+                    grid[i][j]=1;
+                    else{
+                        grid[i][j]=0;
+                    }
 
-        // Iterate through the binary digits in reverse order
-        for (int i = binaryVector.size() - 1; i >= 0; --i) {
-            decimal += binaryVector[i] * power;
-            power *= 2;
+                }
+            }
         }
+        for(int i=0;i<n;i++){
+            int count0=0;
+            int count1=0;
+            for(int j=0;j<m;j++){
+                if(grid[j][i]==0) count0++;
+                else count1++;
+                
+            }
+            if(count1<count0){
+                for(int j=0;j<m;j++){
+                if(grid[j][i]==0) grid[j][i]=1;
+                else grid[j][i]=0;
+                }
+                
+            }
 
-        return decimal;
+            }
+        
+        //into binary
+        
+        for(int i=0;i<m;i++){
+            int pro=1;
+            for(int j=n-1;j>=0;j--){
+                sum+=(pro*grid[i][j]);
+                pro*=2;
+            }
+        }
+        return sum;
+        
     }
 };
