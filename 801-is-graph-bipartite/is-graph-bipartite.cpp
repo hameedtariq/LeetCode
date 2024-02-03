7c6b6ab190b850ece4e948f11556ab166418b80f
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-02-03 16:39:46 +0500

    Time: 12 ms (94.49%) | Memory: 15.9 MB (31.88%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,34 +1,30 @@
-bool _ = []{ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);return false;}();
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        queue<int> q;
-        int size = graph.size();
-        vector<short> visited(size,-1); // mark all nodes as unvisited
-        
-        // run for every node
-        for(int k=0; k< size; k++){
-            // start from node kth node
-            if(visited[k] != -1) continue;
-            q.push(k);
-            int levelSize = 1;
-            int color = 0;
-            while(!q.empty()) {
-                for(int i =0; i< levelSize; i++){
-                    int node = q.front(); q.pop();
-                    visited[node] = color;
-                    for(auto& n: graph[node]){
-                        if(visited[n] == -1) // not visited
-                            q.push(n);
-                        else if(color == visited[n]) // conflict in color of already visited node
-                            return false;
-                    }
+    bool dfs(int i,vector<vector<int>>& graph,vector<int>&color,int col) {
+        color[i]=col;
+        for(auto it:graph[i]){
+            if(color[it]==-1){
+                color[it]=!col;
+                if(!dfs(it,graph,color,!col)){return false;}
+            }
+            else{
+                if(color[it]==col){
+                    return false;
                 }
-                color = color == 0 ? 1 : 0; // change color;
-                levelSize = q.size(); // recalculate level size
             }
         }
-        
-        return true; // all nodes were visited
+        return true;
+
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1);
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if(!dfs(i,graph,color,0)){return false;}
+            }
+        }
+        return true;
+
     }
 };
