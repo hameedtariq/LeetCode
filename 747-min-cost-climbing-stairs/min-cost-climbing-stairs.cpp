b46d93d4482257a5fa9ca3e2155034e79864f2dc
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-02-16 04:17:01 +0500

    Time: 11 ms (10.42%) | Memory: 16.6 MB (5.35%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
+    int memory[1001];
+    vector<int>arr;
+
+    int min_cost(int idx)
+    {
+          if(idx >= (int)arr.size())
+          return 0;
+
+          int &ret = memory[idx];
+          if(ret!=-1)
+          return ret;
+
+
+          int one_step = arr[idx] + min_cost(idx+1);
+          int two_steps = arr[idx] + min_cost(idx+2);
+
+          return ret = min(one_step , two_steps);
+    }
+
     int minCostClimbingStairs(vector<int>& cost) {
-        if(cost.size() == 1) return cost[0];
-        int a = cost[cost.size()-1];
-        int b = 0;
-        for(int i = cost.size() - 2; i >=0; i--){
-            int t = a;
-            a = cost[i] + min(a, b);
-            b = t;
-        }
-        return min(a,b);
+        arr = cost;
+        memset(memory ,-1 , sizeof(memory));
+        return min (min_cost(0) , min_cost(1));     
     }
 };
