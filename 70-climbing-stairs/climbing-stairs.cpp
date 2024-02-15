01b6e66bf8f40e2a720f634bc1f5202e3509600d
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-02-16 03:39:37 +0500

    Time: 2 ms (46.72%) | Memory: 7 MB (20.03%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,11 +1,13 @@
 class Solution {
-    vector<int> memo = vector<int>(46, -1);
 public:
     int climbStairs(int n) {
-        if(n == 0) return 1;
-        if(n < 0) return 0;
-        if(memo[n] != -1) return memo[n];
-        memo[n] = climbStairs(n-1) + climbStairs(n-2);
-        return memo[n];
+        int first = 1;
+        int second = 1;
+        for(int i = n; i>=2; i--){
+            auto temp = second;
+            second = second + first;
+            first = temp;
+        }
+        return second;
     }
 };
