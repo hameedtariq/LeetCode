d92bb089138bb76346c9c417b2c91516511f57c1
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-02-22 09:08:14 +0500

    Time: 6 ms (60.76%) | Memory: 11.5 MB (55.97%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,56 +1,43 @@
-class UnionFind
-{
-private:
-  vector<int> parent;
-  int count;
-
-public:
-  UnionFind(int s)
-  {
-    parent.resize(s);
-    for (int i = 0; i < s; i++)
-    {
-      parent[i] = i;
-    }
-    count = s;
-  }
-  // finds the root parent of a node group and returns it.
-  int find(int x)
-  {
-    if (x != parent[x])
-    {
-      x = find(parent[x]);
-    }
-    return x;
-  }
-  void unify(int x, int y)
-  {
-    int rootX = find(x);
-    int rootY = find(y);
-    if (rootX != rootY)
-    {
-      if (rootX > rootY)
-      {
-        parent[rootY] = rootX;
-      }
-      else
-      {
-        parent[rootX] = rootY;
-      }
-    }
-  }
-};
 class Solution {
 public:
+    vector<int> par;
+    vector<int> treeSize;
+
+    int find_parent(int i){
+        if(par[i] == i){
+            return i;
+        }
+        return par[i] = find_parent(par[i]);
+    }
+
+    void merge(int u, int v){
+        if(treeSize[u] > treeSize[v]){
+            swap(u, v);
+        }
+        par[u] = v;
+        treeSize[v] += treeSize[u];
+    }
+
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        UnionFind uf(1001);
-        for(auto e: edges) {
-            if(uf.find(e[0]) == uf.find(e[1])) {
-                return e;
-            }else {
-                uf.unify(e[0],e[1]);
+        int n = edges.size();
+        par.resize(n+1);
+        treeSize.assign(n+1, 1);
+        for(int i=1; i<=n; i++){
+            par[i] = i;
+        }
+
+        vector<int> ans;
+        for(vector<int> &edge: edges){
+            int p1 = find_parent(edge[0]);
+            int p2 = find_parent(edge[1]);
+            if(p1 != p2){
+                merge(p1, p2);
+            }
+            else{
+                ans.push_back(edge[0]);
+                ans.push_back(edge[1]);
             }
         }
-        return {};
+        return ans;
     }
 };
