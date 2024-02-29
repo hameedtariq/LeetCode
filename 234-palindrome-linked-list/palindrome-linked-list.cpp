7229a07eeb1ccddc93a0e8e3f8308e4617b9739a
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-02-29 12:12:37 +0500

    Time: 142 ms (95.78%) | Memory: 116.6 MB (77.30%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -22,7 +22,7 @@ public:
             // odd
             mid = ceil((float)n/2);
         }
-        for(int i = 0; i<mid-1; i++){
+        for(int i = 0; i<mid; i++){
             p = p->next;
         }
         ln x = reverseLink(p);
