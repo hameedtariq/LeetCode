8e7b0d62c6e15f3cc486813b15fff472b1e343dc
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-03-09 16:49:43 +0500

    Time: 12 ms (15.90%) | Memory: 10.8 MB (75.55%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        for(int i = 0; i<nums1.size(); i++) {
+            int j = 0;
+            while(j < nums2.size() && nums2[j] != nums1[i]) j++; // find nums1[i] in nums2[]
+            while(j < nums2.size()) {
+                if(nums2[j] > nums1[i]) {
+                    ans.push_back(nums2[j]);
+                    break;
+                }
+                j++;
+            }
+            if(j == nums2.size()) {
+                ans.push_back(-1);
+            }
+        }
+        return ans;
+    }
+};
