9ebd922fc5ea0de6cf2a5f45cac74af865b5c168
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-03-12 15:25:24 +0500

    Time: 38 ms (91.43%) | Memory: 24 MB (60.95%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-    bool isval(int i, int j, int n){
-        return (i>=0 && j>=0 && j<n && i<n);
-    }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        if(grid[0][0] == 1) return -1;
-        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
-        dist[0][0] = 1;
-        queue<pair<int,pair<int,int>>> q;
-        q.push({1,{0,0}});
-        while(!q.empty()){
-            int cdist = q.front().first;
-            int i=q.front().second.first,j=q.front().second.second;
-            q.pop();
-            for(int ii=-1;ii<=1;ii++){
-                for(int jj=-1;jj<=1;jj++){
-                    if(!(ii==0 && jj==0) && isval(i+ii,j+jj,n) && grid[ii+i][j+jj] == 0){
-                        if(dist[i+ii][j+jj]>cdist+1){
-                            dist[i+ii][j+jj] = cdist+1;
-                            q.push({cdist+1,{i+ii,j+jj}});
-                        }
+        int row = grid.size();
+        int col = grid[0].size();
+        vector<vector<int>> visit(row, vector<int>(col));
+        queue<pair<int,int>> queue;
+        queue.push({0,0});
+        visit[0][0] = 1;
+         if (grid[0][0] != 0) {
+            return -1;
+        }
+        int length = 1;
+        while (!queue.empty()) {
+            int queueLength = queue.size();
+            for (int i = 0; i < queueLength; ++i) {
+                pair<int,int> currPair = queue.front();
+                queue.pop();
+                int r = currPair.first;
+                int c = currPair.second;
+                if (r == row - 1 && c == col - 1) {
+                    return length;
+                }
+
+                int neighbors[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+                for (int j = 0; j < 8; ++j) {
+                    int newR = r + neighbors[j][0];
+                    int newC = c + neighbors[j][1];
+                    if (newR < 0 || newC < 0 || newR >= row || newC >= col || grid[newR][newC] || visit[newR][newC]) {
+                        continue;
                     }
+                    queue.push({newR, newC});
+                    visit[newR][newC] = 1;
                 }
             }
+            length++;
         }
-        if(dist[n-1][n-1] == INT_MAX) return -1;
-        return dist[n-1][n-1];        
+        return -1;
     }
 };
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
