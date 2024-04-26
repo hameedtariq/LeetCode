0ec3b2272b8722fd7438c48e24aeee8d3b4f532c
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-04-26 13:02:21 +0500

    Time: 25 ms (89.47%) | Memory: 17 MB (86.55%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,19 +1,75 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return minPath(grid, 0,-1, n, dp);
-    }
-    int minPath(vector<vector<int>>& grid, int row, int prevCol, int &n, vector<vector<int>>& dp) {
-        if(row >= n || row < 0 ) return 0;
-        if(dp[row][prevCol+1] != -1) return dp[row][prevCol+1];
-        int minSum = INT_MAX;
-        for(int i =0; i<n;i++) {
-            if(i != prevCol)
-                minSum = min(minSum, grid[row][i] + minPath(grid, row+1, i, n,dp));
+        if (grid.size() == 1) {
+            return grid[0][0];
+        } else if (grid.size() == 2) {
+            return min(grid[0][0] + grid[1][1], grid[0][1] + grid[1][0]);
         }
-        dp[row][prevCol+1] = minSum;
-        return dp[row][prevCol+1];
+        int smallestIndex = -1, secondSmallestIndex = -1, smallestSum = 0, secondSmallestSum = 0;
+        for (auto& row : grid) {
+            auto [firstIndex, firstNum, secondIndex, secondNum, thirdIndex, thirdNum] = getTwoSmallestNums(row);
+            if (smallestIndex != firstIndex && smallestIndex != secondIndex) {
+                secondSmallestIndex = secondIndex;
+                secondSmallestSum = smallestSum + secondNum;
+                smallestIndex = firstIndex;
+                smallestSum += firstNum;
+            } else if (smallestIndex == secondIndex) {
+                if (secondSmallestSum + secondNum < smallestSum + thirdNum) {
+                    secondSmallestSum += secondNum;
+                    secondSmallestIndex = secondIndex;
+                } else {
+                    secondSmallestSum = smallestSum + thirdNum;
+                    secondSmallestIndex = thirdIndex;
+                }
+                smallestSum += firstNum;
+                smallestIndex = firstIndex;
+            } else {
+                if (smallestSum + secondNum < secondSmallestSum + firstNum) {
+                    if (smallestSum + thirdNum < secondSmallestSum + firstNum) {
+                        secondSmallestSum = smallestSum + thirdNum;
+                        secondSmallestIndex = thirdIndex;
+                    } else {
+                        secondSmallestSum += firstNum;
+                        secondSmallestIndex = firstIndex;
+                    }
+                    smallestSum += secondNum;
+                    smallestIndex = secondIndex;
+                } else {
+                    swap(smallestSum, secondSmallestSum);
+                    smallestSum += firstNum;
+                    smallestIndex = firstIndex;
+                    secondSmallestSum += secondNum;
+                    secondSmallestIndex = secondIndex;
+                }
+            }
+            cout << smallestSum << ' ' << secondSmallestSum << endl;
+        }
+        return smallestSum;
+    }
+
+private:
+    array<int, 6> getTwoSmallestNums(vector<int>& arr) {
+        int firstIndex = -1, secondIndex = -1, thirdIndex = -1;
+        int firstVal = 1e9, secondVal = 1e9, thirdVal = 1e9;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] < firstVal) {
+                thirdVal = secondVal;
+                secondVal = firstVal;
+                firstVal = arr[i];
+                thirdIndex = secondIndex;
+                secondIndex = firstIndex;
+                firstIndex = i;
+            } else if (arr[i] < secondVal) {
+                thirdVal = secondVal;
+                secondVal = arr[i];
+                thirdIndex = secondIndex;
+                secondIndex = i;
+            } else if (arr[i] < thirdVal) {
+                thirdVal = arr[i];
+                thirdIndex = i;
+            }
+        }
+        return {firstIndex, firstVal, secondIndex, secondVal, thirdIndex, thirdVal};
     }
 };
