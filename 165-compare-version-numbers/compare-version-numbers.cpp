bbcbc62e89e3bbc2d554da6d058223098619dcbf
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-05-03 18:31:13 +0500

    Time: 3 ms (39.03%) | Memory: 8.1 MB (5.03%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        auto v1 = split(version1,'.');
+        auto v2 = split(version2,'.');
+        int x = 0, y =0;
+        while(x < v1.size() && y < v2.size()) {
+            int a = stoi(v1[x]);
+            int b = stoi(v2[y]);
+            if( a== b) {
+                x++;
+                y++;
+            }else if(a > b) {
+                return 1;
+            }else {
+                return -1;
+            }
+        }
+        while(x < v1.size()){
+            int a = stoi(v1[x]);
+            if(a != 0) return 1;
+            x++;
+        }
+        while(y < v2.size()){
+            int a = stoi(v2[y]);
+            if(a != 0) return -1;
+            y++;
+        }
+        return 0;
+    }
+    vector<string> split(string s, char d) {
+        stringstream ss(s);
+        vector<string> res;
+        string temp;
+        while(getline(ss,temp, '.')) {
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
