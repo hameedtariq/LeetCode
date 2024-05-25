0cc69c5d54f75fcf3dd3fc2dcb3e228f1f0e1b96
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-05-26 01:14:40 +0500

    Time: 4 ms (33.60%) | Memory: 8.5 MB (21.97%) - LeetSync

--- 1316-fizz-buzz-multithreaded/fizz-buzz-multithreaded.cpp
+++ 1316-fizz-buzz-multithreaded/fizz-buzz-multithreaded.cpp
@@ -1,87 +1,72 @@
 class FizzBuzz {
 private:
     int n;
-    int turn;
-    condition_variable cv;
-    mutex mtx;
+    mutex m;
+    condition_variable c;
+    int i;
+
 public:
     FizzBuzz(int n) {
         this->n = n;
-        this->turn = 1;
+        this->i = 1;
     }
 
     // printFizz() outputs "fizz".
     void fizz(function<void()> printFizz) {
-        while(true){
-            unique_lock<mutex> lock(mtx);
-            while(turn<=n && !(turn % 3 == 0 && turn % 5 != 0)) {
-                cv.wait(lock);
+        while(i<=n){
+            unique_lock<mutex> lock(m);
+            while(i<=n && (((i%3) == 0) && ((i%5) != 0)) == 0){
+                c.wait(lock);
             }
-            if(turn > n) {
-                cv.notify_all();
-                return;
-            };
-
-            printFizz();
-            turn++;
-            cv.notify_all();
+            if(i<=n){
+                printFizz();
+                i++;
+            }
+            c.notify_all();
         }
-        
     }
 
     // printBuzz() outputs "buzz".
     void buzz(function<void()> printBuzz) {
-        while(true){
-            unique_lock<mutex> lock(mtx);
-            while(turn<=n && !(turn % 3 != 0 && turn % 5 == 0)) {
-                cv.wait(lock);
+        while(i<=n){
+            unique_lock<mutex> lock(m);
+            while(i<=n && (((i%3) != 0) && ((i%5) == 0)) == 0){
+                c.wait(lock);
             }
-            if(turn > n) {
-                cv.notify_all();
-                return;
-            };
-
-            printBuzz();
-            turn++;
-            cv.notify_all();
+            if(i<=n){
+                printBuzz();
+                i++;
+            }
+            c.notify_all();
         }
-        
     }
 
     // printFizzBuzz() outputs "fizzbuzz".
 	void fizzbuzz(function<void()> printFizzBuzz) {
-        while(true) {
-            unique_lock<mutex> lock(mtx);
-            while(turn<=n && !(turn % 3 == 0 && turn % 5 == 0)) {
-                cv.wait(lock);
+        while(i<=n){
+            unique_lock<mutex> lock(m);
+            while(i<=n && (((i%3) == 0) && ((i%5) == 0)) == 0){
+                c.wait(lock);
             }
-            if(turn > n) {
-                cv.notify_all();
-                return;
-            };
-
-            printFizzBuzz();
-            turn++;
-            cv.notify_all();
+            if(i<=n){
+                printFizzBuzz();
+                i++;
+            }
+            c.notify_all();
         }
-        
     }
 
     // printNumber(x) outputs "x", where x is an integer.
     void number(function<void(int)> printNumber) {
-        while(true){
-            unique_lock<mutex> lock(mtx);
-            while(turn<=n && (turn % 3 == 0 || turn % 5 == 0)) {
-                cv.wait(lock);
+        while(i<=n){
+            unique_lock<mutex> lock(m);
+            while(i<=n && (((i%3) != 0) && ((i%5) != 0)) == 0){
+                c.wait(lock);
             }
-            if(turn > n) {
-                cv.notify_all();
-                return;
-            };
-            printNumber(turn);
-            turn++;
-            cv.notify_all();
+            if(i<=n){
+                printNumber(i++);
+            }
+            c.notify_all();
         }
-        
     }
 };
