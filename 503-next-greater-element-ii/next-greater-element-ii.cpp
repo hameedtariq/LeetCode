74ce671e668cfe06446e8d53c0771d73c98a47bb
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-06-04 17:52:23 +0500

    Time: 31 ms (15.72%) | Memory: 26.5 MB (83.71%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n,-1);
+        // unordered_map<int, int> m;
+        stack<int> s;
+        for(int i =0; i<2*n; i++) {
+            while(!s.empty() && nums[s.top()] < nums[i%n]) {
+                ans[s.top()] = nums[i%n];
+                s.pop();
+            }
+            s.push(i%n);
+        }
+
+        
+        return ans;
+    }
+};
