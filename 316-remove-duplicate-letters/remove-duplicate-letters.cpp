a1c20450cca47e0e7b79eb53b99a5196c15b18c8
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-06-04 16:05:22 +0500

    Time: 5 ms (30.27%) | Memory: 8.5 MB (47.15%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        unordered_map<char, int> count;
         stack<char> st;
-        vector<bool> isInStack(26, false);
-
-        for (auto i : s)
-        {
-            count[i]++;
+        vector<int> count(26, 0);
+        for(auto &i: s) {
+            count[i-'a']++;
         }
-
-        for (int i = 0; i < s.length(); i++)
-        {
-            count[s[i]]--;
-
-            if (isInStack[s[i] - 'a']) continue;
-
-            while (!st.empty() && st.top() > s[i] && count[st.top()] > 0) 
-            {
-                isInStack[st.top() - 'a'] = false;
+        vector<bool> inStack(26,false);
+        for(int i = 0; i < s.size(); i++) {
+            count[s[i]-'a']--;
+            if(inStack[s[i]-'a']) continue;
+            while(!st.empty() && st.top() > s[i] && count[st.top()-'a'] > 0){
+                inStack[st.top()-'a'] = false;
                 st.pop();
             }
-
             st.push(s[i]);
-            isInStack[s[i] - 'a'] = true;
+            inStack[s[i]-'a'] = true;
         }
-
-        string ans = "";
-        while (!st.empty())
-        {
-            ans += st.top();
-            cout << st.top() << " ";
+        string ans;
+        while(!st.empty()) {
+            ans+= st.top();
             st.pop();
         }
 
-        reverse(ans.begin(), ans.end());
-
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
