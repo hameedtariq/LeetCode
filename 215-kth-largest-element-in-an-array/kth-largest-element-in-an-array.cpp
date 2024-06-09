a05117aff503de726be849d6fa23c5fb3b973bef
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-06-09 17:08:10 +0500

    Time: 76 ms (90.47%) | Memory: 64.3 MB (11.52%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        // std::priority_queue<int> h;
-        // for (auto num: nums) {
-        //     h.push(num);
-        // }
-        // for (int i=0; i<k-1; i++)
-        //     h.pop();
-        // return h.top();
+
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
+
+        std::priority_queue<int> h;
+        for (auto num: nums) {
+            h.push(num);
+        }
+        for (int i=0; i<k-1; i++)
+            h.pop();
+        return h.top();
 
 
 
-        auto f = nums.begin();
-        auto l = nums.end();
-
-        k = nums.size() - k;
-
-        nth_element(f, f + k, l);
-
-        return *(f + k);
+        
     }
 };
