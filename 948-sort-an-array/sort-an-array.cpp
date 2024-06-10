152da40986800f467c87ee32f16d343160221a18
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-06-10 23:43:53 +0500

    Time: 1887 ms (5.02%) | Memory: 74.6 MB (55.70%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
+        quickSort(nums, 0, nums.size()-1);
         return nums;
     }
-    void mergeSort(vector<int>& nums, int left, int right) {
+    void quickSort(vector<int>& nums, int left, int right) {
         if(left >= right) return;
-        int mid = left + (right-left)/2;
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid+1, right);
-        
-        merge(nums, left, mid, right);        
-    }
-
-    void merge(vector<int>& nums, int left, int mid, int right){
+        int pivot = (rand() % (right-left+1)) + left;
+        swap(nums[pivot], nums[right]);
         int l = left;
-        int r = mid+1;
-        vector<int> temp(right-left+1);
-        int k = 0;
-        // merge sorted sub arrays into new array.
-        while(l <= mid && r <= right){
-            if(nums[l] <= nums[r]) {
-                temp[k++] = nums[l++];
+        int r = right-1;
+        while(l <= r) {
+            if(nums[l] <= nums[right]) {
+                l++;
+            }else if(nums[r] >= nums[right]) {
+                r--;
             }else {
-                temp[k++] = nums[r++];
+                swap(nums[l], nums[r]);
+                l++;
+                r--;
             }
         }
-        while(l <= mid){
-            temp[k++] = nums[l++];
-        }
-        while(r <= right){
-            temp[k++] = nums[r++];
-        }
-        // fill back sorted portion into array.
-        for(int i = 0; i<temp.size(); i++) {
-            nums[left+i] = temp[i];
-        }
+        swap(nums[l], nums[right]);
+        quickSort(nums, left, l-1);
+        quickSort(nums, l+1, right);
     }
 };
