3423d91ace5b3a0a8effe142b9ededc926720457
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-06-16 14:42:42 +0500

    Time: 4 ms (94.04%) | Memory: 22.3 MB (82.09%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int size = height.size();
-        if (size == 0) return 0;
-
         int left = 0;
-        int right = size - 1;
-        int left_max = 0;
-        int right_max = 0;
-        int totalVol = 0;
-
-        while (left <= right) {
-            if (height[left] < height[right]) {
-                if (height[left] >= left_max) {
-                    left_max = height[left];
-                } else {
-                    totalVol += left_max - height[left];
-                }
+        int left_max = height[left];
+        int right = height.size() - 1;
+        int right_max = height[right];
+        int vol = 0;
+        while(left < right) {
+            if(left_max < right_max) {
                 left++;
-            } else {
-                if (height[right] >= right_max) {
-                    right_max = height[right];
-                } else {
-                    totalVol += right_max - height[right];
-                }
+                left_max = max(left_max, height[left]);
+                vol += left_max - height[left];
+            }else {
                 right--;
+                right_max = max(right_max, height[right]);
+                vol += right_max - height[right];
             }
         }
-
-        return totalVol;
+        return vol;
     }
 };
