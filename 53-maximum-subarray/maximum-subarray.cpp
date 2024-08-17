d05644068cd379e22d8933967ae1057d7e3f05f6
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-08-17 06:52:33 +0500

    Time: 79 ms (46.96%) | Memory: 70.5 MB (57.44%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,24 +1,12 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
-int speedUp = [] {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
-    return 0;
-}();
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_global = nums[0];
-        int max_local = nums[0];
+        int globalSum = nums[0];
+        int localSum = nums[0];
         for(int i =1; i<nums.size(); i++) {
-            max_local = max(nums[i], nums[i]+max_local);
-            max_global = max(max_local, max_global);
+            localSum = max(localSum+nums[i],nums[i]);
+            globalSum = max(localSum, globalSum);
         }
-        return max_global;
+        return globalSum;
     }
 };
