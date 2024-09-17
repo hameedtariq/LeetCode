45baadfcb10679ee76c68db5b8ce4b20685b5a3c
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-09-18 02:55:52 +0500

    Time: 4 ms (41.82%) | Memory: 13.9 MB (81.27%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,59 +1,35 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1, n = nums.size();
-        int pi = -1;
-        
-        // Find the pivot
+        int l = 0, r = nums.size() - 1;
+
         while (l <= r) {
             int mid = (l + r) / 2;
-            
-            // Check if mid is pivot
-            if (mid > 0 && nums[mid] < nums[mid - 1]) {
-                pi = mid;
-                break;
-            } 
-            if (mid < n - 1 && nums[mid] > nums[mid + 1]) {
-                pi = mid + 1;
-                break;
-            }
 
-            // Decide which side to go
-            if (nums[mid] >= nums[l]) {
-                // Go right
-                l = mid + 1;
-            } else {
-                // Go left
-                r = mid - 1;
-            }
-        }
-        
-        // If pivot is not found, the array is not rotated, apply binary search on the whole array
-        if (pi == -1) {
-            return bs(nums, target, 0, n - 1);
-        }
-        
-        // Search in the left half
-        if (target >= nums[0] && target <= nums[pi - 1]) {
-            return bs(nums, target, 0, pi - 1);
-        }
-        
-        // Search in the right half
-        return bs(nums, target, pi, n - 1);
-    }
-
-private:
-    int bs(vector<int>& nums, int target, int l, int r) {
-        while (l <= r) {
-            int mid = (l + r) / 2;
+            // Check if the middle element is the target
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) {
-                l = mid + 1;
+            }
+
+            // Determine which half is sorted
+            if (nums[l] <= nums[mid]) {
+                // Left half is sorted
+                if (target >= nums[l] && target < nums[mid]) {
+                    r = mid - 1; // Target is in the sorted left half
+                } else {
+                    l = mid + 1; // Target is in the right half
+                }
             } else {
-                r = mid - 1;
+                // Right half is sorted
+                if (target > nums[mid] && target <= nums[r]) {
+                    l = mid + 1; // Target is in the sorted right half
+                } else {
+                    r = mid - 1; // Target is in the left half
+                }
             }
         }
+        
+        // Target not found
         return -1;
     }
 };
