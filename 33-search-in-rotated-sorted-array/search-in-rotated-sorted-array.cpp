5104a1609b8b3dde2595d2915dbd99dd19847c80
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-09-18 02:53:06 +0500

    Time: 4 ms (41.82%) | Memory: 14 MB (48.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,8 +1,58 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i =0; i<nums.size(); i++) {
-            if(nums[i] == target) return i;
+        int l = 0, r = nums.size() - 1, n = nums.size();
+        int pi = -1;
+        
+        // Find the pivot
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            
+            // Check if mid is pivot
+            if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                pi = mid;
+                break;
+            } 
+            if (mid < n - 1 && nums[mid] > nums[mid + 1]) {
+                pi = mid + 1;
+                break;
+            }
+
+            // Decide which side to go
+            if (nums[mid] >= nums[l]) {
+                // Go right
+                l = mid + 1;
+            } else {
+                // Go left
+                r = mid - 1;
+            }
+        }
+        
+        // If pivot is not found, the array is not rotated, apply binary search on the whole array
+        if (pi == -1) {
+            return bs(nums, target, 0, n - 1);
+        }
+        
+        // Search in the left half
+        if (target >= nums[0] && target <= nums[pi - 1]) {
+            return bs(nums, target, 0, pi - 1);
+        }
+        
+        // Search in the right half
+        return bs(nums, target, pi, n - 1);
+    }
+
+private:
+    int bs(vector<int>& nums, int target, int l, int r) {
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
         return -1;
     }
