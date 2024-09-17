fb8001f5c1959646cd355689f7b1f1af268a4d15
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-09-18 04:16:23 +0500

    Time: 7 ms (47.68%) | Memory: 16.1 MB (99.53%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,39 +2,26 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans(2, -1);
-        ans[0] = findFirst(nums, target);
-        ans[1] = findLast(nums, target);
+        bs(nums, target, ans, 0, nums.size() - 1);
         return ans;
     }
 
 private:
-    int findFirst(vector<int>& nums, int target) {
-        int index = -1;
-        int l = 0, r = nums.size() - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] >= target) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-            if (nums[mid] == target) index = mid;
-        }
-        return index;
-    }
+    void bs(vector<int>& nums, int target, vector<int>& ans, int l, int r) {
+        if (l > r) return;
+        int mid = l + (r - l) / 2;
 
-    int findLast(vector<int>& nums, int target) {
-        int index = -1;
-        int l = 0, r = nums.size() - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] <= target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-            if (nums[mid] == target) index = mid;
+        if (nums[mid] == target) {
+            // Update ans[0] and ans[1] if needed
+            if (ans[0] == -1 || mid < ans[0]) ans[0] = mid;
+            if (ans[1] == -1 || mid > ans[1]) ans[1] = mid;
+            // Search both left and right halves
+            bs(nums, target, ans, l, mid - 1);
+            bs(nums, target, ans, mid + 1, r);
+        } else if (nums[mid] < target) {
+            bs(nums, target, ans, mid + 1, r);
+        } else {
+            bs(nums, target, ans, l, mid - 1);
         }
-        return index;
     }
 };
