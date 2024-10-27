af7fb116f367ce15dc033d8c046b36451960e178
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-10-27 19:18:55 +0500

    Time: 37 ms (62.11%) | Memory: 17.3 MB (85.63%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,34 +9,13 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int s1 = 1, s2 = 1;
         auto p = headA;
-        while(p->next) {
-            p = p->next;
-            s1++;        
-        }
         auto q = headB;
-        while(q->next) {
-            q = q->next;
-            s2++;        
+        // Traverse both lists, switching pointers to the other list's head when reaching the end
+        while (p != q) {
+            p = p ? p->next : headB;  // Move p, or reset to headB if at the end of list A
+            q = q ? q->next : headA;  // Move q, or reset to headA if at the end of list B
         }
-
-        if(p != q) return nullptr;
-
-        p = headA, q= headB;
-        while(p && q) {
-            if(p == q) return p;
-            if(s1 > s2) {
-                s1--;
-                cout << "P: " <<p->val << endl;
-                p = p->next;
-            }else {
-                s2--;
-                cout << "Q: " << q->val << endl;
-                q = q->next;
-            }
-        }
-
-        return nullptr;
+        return p;
     }
 };
