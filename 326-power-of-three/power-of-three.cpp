cde6597540f0f43067ec47e2ec7ecfc99f50ac07
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-10-28 14:39:37 +0500

    Time: 0 ms (100.00%) | Memory: 8.3 MB (88.16%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n < 1) {
+            return false;
+        }
+        while(n >= 3) {
+            if(n%3 == 0) {
+                n /= 3;
+            }else {
+                return false;
+            }
+        }
+
+        if(n == 1) return true;
+
+        return false;
+    }
+};
