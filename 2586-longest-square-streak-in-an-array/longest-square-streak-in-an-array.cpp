b12fca738d50cbb0730911ff9f2a97373871f110
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-10-29 17:41:39 +0500

    Time: 4 ms (91.43%) | Memory: 85.4 MB (96.32%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -1,26 +1,36 @@
+const int BS_SIZE = 100'001;
+bitset<BS_SIZE> bs;
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        unordered_set<long long> ss;
-
-        sort(nums.begin(), nums.end());
-
-        for(auto&n: nums) {
-            ss.insert(n);
+        int start = INT_MAX;
+        for(int num : nums) {
+            bs[num] = 1;
+            start = min(start, num);
         }
-        int length = 1;
+
+        int ans = -1;
         
-        for(auto&n: nums) {
-            long long x = pow(n,2);
-            int l = 1;
-            while(ss.find(x) != ss.end()) {
-                ss.erase(x);
-                l++;
-                x = pow(x, 2);
+        for(int i = start; i != BS_SIZE; i = bs._Find_next(i)) {
+            bs[i] = 0; // auto-reset
+            if(i > 316) continue;
+            int num = i * i;
+            if(!bs[num]) continue;
+            bs[num] = 0;
+            int j = 2;
+            while(num <= 316) { // 316 = floor(sqrt(1e5)), where 1e5 is max constraint
+                num *= num;
+                if(bs[num]) {
+                    j++, bs[num] = 0; // auto-reset
+                }else {
+                    break;
+                }
             }
-            length = max(length, l);
+
+            ans = max(ans, j);
         }
 
-        return length == 1 ? -1 : length;
+
+        return ans;
     }
 };
