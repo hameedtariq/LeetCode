18003badc0e375686e980a13fa2c155bfa82952f
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-11-23 18:53:04 +0500

    Time: 306 ms (84.08%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,2 +1,7 @@
 -- Write your PostgreSQL query statement below
-SELECT m.name from Employee e JOIN Employee m ON e.managerId = m.id GROUP BY e.managerId, m.name HAVING COUNT(e.managerId) > 4;
+SELECT m.name 
+FROM Employee m 
+LEFT JOIN Employee e 
+ON e.managerId = m.id 
+GROUP BY m.id, m.name 
+HAVING COUNT(m.id) > 4;
