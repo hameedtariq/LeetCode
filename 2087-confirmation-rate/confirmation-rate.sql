831d47eb6aca87de7820d7804199fba9e983c263
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-11-23 19:26:13 +0500

    Time: 259 ms (74.55%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,7 +1,6 @@
-/* Write your PL/SQL query statement below */
-select s.user_id, ROUND(count(case action when 'confirmed' then 1 else null end)/count(*), 2) as confirmation_rate
--- select *
-from Confirmations c
-right join Signups s
-on c.user_id = s.user_id
-group by s.user_id;
+-- Write your PostgreSQL query statement below
+SELECT s.user_id, ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END), 2) as confirmation_rate
+FROM Signups s 
+LEFT JOIN Confirmations c 
+USING (user_id)
+GROUP BY s.user_id
