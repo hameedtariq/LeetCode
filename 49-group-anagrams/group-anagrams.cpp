087ae3a208f68c9d259298abbf8001100c1ba1b8
Muhammad Hameed <69725350+hameedtariq@users.noreply.github.com>
2024-12-02 02:10:11 +0500

    Time: 11 ms (95.00%) | Memory: 21.5 MB (99.08%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
+    inline uint hash(vector<int> &V){ 
+        uint ans = 0; 
+        for(auto x : V){ 
+            ans *= 131; 
+            ans += x; 
+        }
+        return ans; 
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> m;
-        vector<int> v(26);
-        for(auto&str: strs) {
-            fill(v.begin(), v.end(), 0);
-            for(auto& c: str) {
-                v[c-'a']++;
-            }
-            string key = to_string(v[0]);
-            for (int i = 1; i < 26; ++i) {
-                key += ',' + to_string(v[i]);
-            }
-            m[key].push_back(str);
+        vector<int> hist(26, 0); 
+        vector<pair<uint, int> > V;  
+        int ind = 0; 
+        for(string str : strs){ 
+            fill(hist.begin(), hist.end(), 0); 
+            for(char c: str) 
+                hist[c-'a']++;
+            V.push_back({hash(hist), ind++}); 
         }
-        vector<vector<string>> res;
-        for(auto& pair: m) {
-            res.push_back(pair.second);
+        sort(V.begin(), V.end());  
+        vector<vector<string> >ans; 
+        ans.push_back({strs[V[0].second]});  
+        for(int i = 1; i < V.size(); i++){ 
+            if(V[i].first == V[i-1].first)
+                ans.back().push_back(strs[V[i].second]); 
+            else 
+                ans.push_back({strs[V[i].second]}); 
         }
-        return res;
+        return ans; 
     }
 };
